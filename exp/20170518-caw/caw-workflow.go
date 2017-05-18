673bbc9ded9c458a4c21646ec668b8cdf40b2dd2
Samuel Lampa <samuel.lampa@gmail.com>
2017-05-18 14:46:26 +0200

    Put data in data folder

--- exp/20170518-caw/caw-workflow.go
+++ exp/20170518-caw/caw-workflow.go
@@ -12,21 +12,21 @@ func main() {
 	// Init processes
 
 	dlApps := sp.NewFromShell("dlApps", "wget http://uppnex.se/apps.tar.gz -O {o:apps}")
-	dlApps.SetPathStatic("apps", "uppnex_apps.tar.gz")
+	dlApps.SetPathStatic("apps", "data/uppnex_apps.tar.gz")
 	runner.AddProcess(dlApps)
 
 	unzipApps := sp.NewFromShell("unzipApps", "zcat {i:targz} > {o:tar}")
 	unzipApps.SetPathReplace("targz", "tar", ".gz", "")
 	runner.AddProcess(unzipApps)
 
-	untarApps := sp.NewFromShell("untarApps", "tar -xvf {i:tar} # {o:outdir}")
-	untarApps.SetPathStatic("outdir", "apps")
+	untarApps := sp.NewFromShell("untarApps", "tar -xvf {i:tar} -C data # {o:outdir}")
+	untarApps.SetPathStatic("outdir", "data/apps")
 	runner.AddProcess(untarApps)
 
 	sink := sp.NewSink()
 	runner.AddProcess(sink)
 
-	// Connect network
+	// Connect dependencies
 
 	unzipApps.In["targz"].Connect(dlApps.Out["apps"])
 	untarApps.In["tar"].Connect(unzipApps.Out["tar"])
