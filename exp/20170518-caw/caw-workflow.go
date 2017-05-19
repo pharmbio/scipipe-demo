a95841427978ee1e9af1c2d9376396348966ac3f
Samuel Lampa <samuel.lampa@gmail.com>
2017-05-19 18:50:48 +0200

    Implement alignReads component (including new ParamQueue comp)

--- exp/20170518-caw/caw-workflow.go
+++ exp/20170518-caw/caw-workflow.go
@@ -7,34 +7,126 @@ import (
 func main() {
 	sp.InitLogInfo()
 
+	// Set up paths
+	// ------------------------------------------------
+	// SCRATCHDIR=$(readlink -f $1)
+	// APPSDIR=$(readlink -f $2)
+	// REFDIR=$(readlink -f $3)
+	// DATADIR=$(readlink -f $4)
+
+	scratchDir := "tmp"
+	appsDir := "data/apps"
+	refDir := appsDir + "/pipeline_test/ref"
+	origDataDir := appsDir + "/pipeline_test/ref"
+	dataDir := "data"
+
+	// ================================================================================
+	// Initial data download workflow
+	// ================================================================================
+
 	// Init processes
 
-	runner := sp.NewPipelineRunner()
+	dlDataRun := sp.NewPipelineRunner()
 
 	dlApps := sp.NewFromShell("dlApps", "wget http://uppnex.se/apps.tar.gz -O {o:apps}")
-	dlApps.SetPathStatic("apps", "data/uppnex_apps.tar.gz")
-	runner.AddProcess(dlApps)
+	dlApps.SetPathStatic("apps", dataDir+"/uppnex_apps.tar.gz")
+	dlDataRun.AddProcess(dlApps)
 
 	unzipApps := sp.NewFromShell("unzipApps", "zcat {i:targz} > {o:tar}")
 	unzipApps.SetPathReplace("targz", "tar", ".gz", "")
-	runner.AddProcess(unzipApps)
+	dlDataRun.AddProcess(unzipApps)
 
-	untarApps := sp.NewFromShell("untarApps", "tar -xvf {i:tar} -C data # {o:outdir}")
-	untarApps.SetPathStatic("outdir", "data/apps")
-	runner.AddProcess(untarApps)
+	untarApps := sp.NewFromShell("untarApps", "tar -xvf {i:tar} -C "+dataDir+" # {o:outdir}")
+	untarApps.SetPathStatic("outdir", dataDir+"/apps")
+	dlDataRun.AddProcess(untarApps)
 
-	sink := sp.NewSink()
-	runner.AddProcess(sink)
+	dlDataSink := sp.NewSink()
+	dlDataRun.AddProcess(dlDataSink)
 
 	// Connect dependencies
 
 	sp.Connect(unzipApps.In["targz"], dlApps.Out["apps"])
 	sp.Connect(untarApps.In["tar"], unzipApps.Out["tar"])
-	sink.Connect(untarApps.Out["outdir"])
+	dlDataSink.Connect(untarApps.Out["outdir"])
 
 	// Run
 
-	runner.Run()
+	dlDataRun.Run()
+
+	// # align samples
+	// echo -e "\naligning normal 1\n"
+	// bwa mem -R "@RG\tID:normal_1\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L001_R1.fastq.gz $DATADIR/tiny_normal_L001_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - | samtools sort - > $SCRATCHDIR/normal_1.bam
+	// echo -e "\naligning normal 2\n"
+	// bwa mem -R "@RG\tID:normal_2\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L002_R1.fastq.gz $DATADIR/tiny_normal_L002_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - | samtools sort - > $SCRATCHDIR/normal_2.bam
+	// echo -e "\naligning normal 4\n"
+	// bwa mem -R "@RG\tID:normal_4\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L004_R1.fastq.gz $DATADIR/tiny_normal_L004_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - |   samtools sort - > $SCRATCHDIR/normal_4.bam
+	// echo -e "\naligning normal 7\n"
+	// bwa mem -R "@RG\tID:normal_7\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L007_R1.fastq.gz $DATADIR/tiny_normal_L007_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - |   samtools sort - > $SCRATCHDIR/normal_7.bam
+	// echo -e "\naligning normal 8\n"
+	// bwa mem -R "@RG\tID:normal_8\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L008_R1.fastq.gz $DATADIR/tiny_normal_L008_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - |   samtools sort - > $SCRATCHDIR/normal_8.bam
+
+	//alignSamples.SetPathStatic("reads1", dataDir+"/tiny_normal_L001_R1.fastq.gz")
+	//alignSamples.SetPathStatic("reads2", dataDir+"/tiny_normal_L001_R2.fastq.gz")
+
+	// ================================================================================
+	// Main Workflow
+	// ================================================================================
+
+	mainWfRun := sp.NewPipelineRunner()
+
+	// --------------------------------------------------------------------------------
+	// Align Samples
+	// --------------------------------------------------------------------------------
+	refFasta := refDir + "/human_g1k_v37_decoy.fasta"
+	refIndex := refDir + "/human_g1k_v37_decoy.fasta"
+
+	// Define process
+	alignSamples := sp.NewFromShell("alignSamples", "bwa mem -R \"@RG\tID:normal_{p:index}\tSM:normal\tLB:normal\tPL:illumina\" -B 3 -t 4 -M "+refFasta+" {i:reads1} {i:reads2}"+
+		"| samtools view -bS -t "+refIndex+" - "+
+		"| samtools sort - > {o:bam}")
+
+	// Create output file format
+	alignSamples.PathFormatters["bam"] = func(t *sp.SciTask) string {
+		outPath := scratchDir + "/normal_" + t.Params["index"] + ".bam"
+		return outPath
+	}
+	mainWfRun.AddProcess(alignSamples)
+
+	// Loop over indexes, and create parameters and file paths and send to alignSamples
+	indexes := []string{"1", "2", "4", "7", "8"}
+	reads1Paths := []string{}
+	reads2Paths := []string{}
+	for _, idx := range indexes {
+		reads1Paths = append(reads1Paths, origDataDir+"/tiny_normal_L00"+idx+"_R1.fastq.gz")
+		reads2Paths = append(reads2Paths, origDataDir+"/tiny_normal_L00"+idx+"_R2.fastq.gz")
+	}
+
+	reads1FileQueue := sp.NewIPQueue(reads1Paths...)
+	mainWfRun.AddProcess(reads1FileQueue)
+
+	reads2FileQueue := sp.NewIPQueue(reads1Paths...)
+	mainWfRun.AddProcess(reads2FileQueue)
+
+	readsIndexQueue := NewParamQueue(indexes...)
+	mainWfRun.AddProcess(readsIndexQueue)
+
+	// --------------------------------------------------------------------------------
+
+	mainWfSink := sp.NewSink()
+	mainWfRun.AddProcess(mainWfSink)
+
+	// --------------------------------------------------------------------------------
+	// Connect network
+	// --------------------------------------------------------------------------------
+	readsIndexQueue.Out.Connect(alignSamples.ParamPorts["index"])
+	sp.Connect(reads1FileQueue.Out, alignSamples.In["reads1"])
+	sp.Connect(reads2FileQueue.Out, alignSamples.In["reads2"])
+	mainWfSink.Connect(alignSamples.Out["bam"])
+
+	// --------------------------------------------------------------------------------
+	// Run main workflow
+	// --------------------------------------------------------------------------------
+	mainWfRun.Run()
 }
 
 // Martin's original script below:
@@ -72,7 +164,6 @@ func main() {
 // # align samples
 // echo -e "\naligning normal 1\n"
 // bwa mem -R "@RG\tID:normal_1\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L001_R1.fastq.gz $DATADIR/tiny_normal_L001_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - | samtools sort - > $SCRATCHDIR/normal_1.bam
-//
 // echo -e "\naligning normal 2\n"
 // bwa mem -R "@RG\tID:normal_2\tSM:normal\tLB:normal\tPL:illumina" -B 3 -t 4 -M $REFDIR/human_g1k_v37_decoy.fasta $DATADIR/tiny_normal_L002_R1.fastq.gz $DATADIR/tiny_normal_L002_R2.fastq.gz |   samtools view -bS -t $REFDIR/human_g1k_v37_decoy.fasta.fai - | samtools sort - > $SCRATCHDIR/normal_2.bam
 // echo -e "\naligning normal 4\n"
@@ -116,3 +207,27 @@ func main() {
 //
 // # restore path
 // export PATH=$PATHBAK
+
+type ParamQueue struct {
+	sp.Process
+	Out    *sp.ParamPort
+	params []string
+}
+
+func NewParamQueue(params ...string) *ParamQueue {
+	return &ParamQueue{
+		Out:    sp.NewParamPort(),
+		params: params,
+	}
+}
+
+func (p *ParamQueue) Run() {
+	defer p.Out.Close()
+	for _, param := range p.params {
+		p.Out.Chan <- param
+	}
+}
+
+func (p *ParamQueue) IsConnected() bool {
+	return p.Out.IsConnected()
+}
