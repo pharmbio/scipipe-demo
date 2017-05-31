3501d3f8e97bcf5e7753f91ad39f6bf887d940cf
Samuel Lampa <samuel.lampa@gmail.com>
2017-05-31 13:54:51 +0200

    Don't load nonexistant GATK module and log to file when running

--- exp/20170518-caw/run.sh
+++ exp/20170518-caw/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
-module load bioinfo-tools; module load bwa/0.7.15 samtools/1.4 GATK/3.6
-go run caw-workflow.go
+module load bioinfo-tools; module load bwa/0.7.15 samtools/1.4
+time go run caw-workflow.go 2>&1 | tee run-$(date +%Y%m%d-%H%M%S).log
