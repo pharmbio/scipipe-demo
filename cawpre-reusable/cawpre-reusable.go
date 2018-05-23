e77fee6250cc51e98eab5acbcb027e4d5f51df51
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-23 14:34:56 +0200

    Update cawpre-reusable workflow to latest SciPipe API too

--- cawpre-reusable/cawpre-reusable.go
+++ cawpre-reusable/cawpre-reusable.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	. "github.com/scipipe/scipipe"
-	spcomp "github.com/scipipe/scipipe/components"
 	"strconv"
 	"strings"
+
+	. "github.com/scipipe/scipipe"
+	spcomp "github.com/scipipe/scipipe/components"
 )
 
 // Set up some constant stuff like paths
@@ -29,14 +30,16 @@ var (
 
 func main() {
 
+	coresCnt := 4
+
 	// Run the Data Download part of the workflow
-	downloadDataWorkflow := NewDownloadWorkflow(dataDir)
+	downloadDataWorkflow := NewDownloadWorkflow(dataDir, coresCnt)
 	downloadDataWorkflow.Run()
 
 	// ------------------------------------------------
 	// Main workflow starts here
 	// ------------------------------------------------
-	wf := NewWorkflow("caw_wf")
+	wf := NewWorkflow("caw_wf", coresCnt)
 
 	// Init a process "holder" for the final process in this part, as we need
 	// to access the normal and tumor verions specifically
@@ -46,9 +49,6 @@ func main() {
 		sampleIdx := strconv.Itoa(sampleIdxStr)
 
 		// Some parameter book-keeping
-		indexGen := spcomp.NewStringGen("gen_readsidxes_"+sampleType, readsIndexes[sampleType]...)
-		wf.Add(indexGen)
-
 		readsPaths1 := []string{}
 		readsPaths2 := []string{}
 		for _, readsIdx := range readsIndexes[sampleType] {
@@ -56,25 +56,22 @@ func main() {
 			readsPaths2 = append(readsPaths2, origDataDir+"/tiny_"+sampleType+"_L00"+readsIdx+"_R2.fastq.gz")
 		}
 
+		readsSrc1 := spcomp.NewFileSource(wf, "gen_readspaths1_"+sampleType, readsPaths1...)
+		readsSrc2 := spcomp.NewFileSource(wf, "gen_readspaths2_"+sampleType, readsPaths2...)
+		indexSrc := spcomp.NewParamSource(wf, "gen_readsidxes_"+sampleType, readsIndexes[sampleType]...)
+
 		// Align samples
-		readsGen1 := NewIPGen("gen_readspaths1_"+sampleType, readsPaths1...)
-		wf.Add(readsGen1)
-
-		readsGen2 := NewIPGen("gen_readspaths2_"+sampleType, readsPaths2...)
-		wf.Add(readsGen2)
-
 		alignSamples := NewBwaAlign(wf, "align_samples", sampleType, refFasta, refIndex)
-		alignSamples.InReads1().Connect(readsGen1.Out)
-		alignSamples.InReads2().Connect(readsGen2.Out)
-		alignSamples.ParamIndexNo().Connect(indexGen.Out)
+		alignSamples.InReads1().Connect(readsSrc1.Out())
+		alignSamples.InReads2().Connect(readsSrc2.Out())
+		alignSamples.ParamIndexNo().Connect(indexSrc.Out())
 
 		// Merge BAMs
-		streamToSubstream := spcomp.NewStreamToSubStream("alignsamples_str2substr_" + sampleType)
-		streamToSubstream.In.Connect(alignSamples.OutBam())
-		wf.Add(streamToSubstream)
+		streamToSubstream := spcomp.NewStreamToSubStream(wf, "alignsamples_str2substr_"+sampleType)
+		streamToSubstream.In().Connect(alignSamples.OutBam())
 
 		mergeBams := NewSamtoolsMerge(wf, "merge_bams", sampleType, tmpDir)
-		mergeBams.InBams().Connect(streamToSubstream.OutSubStream)
+		mergeBams.InBams().Connect(streamToSubstream.OutSubStream())
 
 		// Mark Duplicates
 		markDupes := NewGATKMarkDuplicates(wf, "mark_duplicates", sampleType, sampleIdx, appsDir, tmpDir)
@@ -102,7 +99,6 @@ func main() {
 		printReads := NewGATKPrintReads(wf, "print_reads", sampleType, appsDir, refDir)
 		printReads.In("realbam").Connect(realignIndels.Out("realbam" + sampleType))
 		printReads.In("recaltable").Connect(reCalibrate.Out("recaltable"))
-		wf.ConnectLast(printReads.Out("recalbam"))
 	}
 
 	// Run workflow
@@ -121,8 +117,8 @@ type DownloadWorkflow struct {
 	*Workflow
 }
 
-func NewDownloadWorkflow(dataDir string) *DownloadWorkflow {
-	wf := &DownloadWorkflow{NewWorkflow("dldata_wf")}
+func NewDownloadWorkflow(dataDir string, cores int) *DownloadWorkflow {
+	wf := &DownloadWorkflow{NewWorkflow("dldata_wf", cores)}
 
 	dwnld := wf.NewProc("download_apps", "wget http://uppnex.se/apps.tar.gz -O {o:apps}")
 	dwnld.SetPathStatic("apps", dataDir+"/uppnex_apps.tar.gz")
@@ -134,8 +130,6 @@ func NewDownloadWorkflow(dataDir string) *DownloadWorkflow {
 	untar := wf.NewProc("untar_apps", "tar -xvf {i:tar} -C "+dataDir+" # {o:outdir}")
 	untar.SetPathStatic("outdir", dataDir+"/apps")
 	untar.In("tar").Connect(unzip.Out("tar"))
-
-	wf.ConnectLast(untar.Out("outdir"))
 
 	return wf
 }
@@ -149,32 +143,31 @@ func NewDownloadWorkflow(dataDir string) *DownloadWorkflow {
 // ----------------------------------------------------------------------------
 
 type BwaAlign struct {
-	*SciProcess
+	*Process
 }
 
 func NewBwaAlign(wf *Workflow, procName string, sampleType string, refFasta string, refIndex string) *BwaAlign {
-	sampleTyp := sampleType
 	innerProc := wf.NewProc(procName+"_"+sampleType, "bwa mem -R \"@RG\tID:"+sampleType+"_{p:indexno}\tSM:"+sampleType+"\tLB:"+sampleType+"\tPL:illumina\" -B 3 -t 4 -M "+refFasta+" {i:reads_1} {i:reads_2}"+
 		"| samtools view -bS -t "+refIndex+" - "+
 		"| samtools sort - > {o:bam}")
-	innerProc.SetPathCustom("bam", func(t *SciTask) string {
-		outPath := tmpDir + "/" + sampleTyp + "_" + t.Params["indexno"] + ".bam"
+	innerProc.SetPathCustom("bam", func(t *Task) string {
+		outPath := tmpDir + "/" + sampleType + "_" + t.Params["indexno"] + ".bam"
 		return outPath
 	})
 	return &BwaAlign{innerProc}
 }
 
-func (p *BwaAlign) ParamIndexNo() *ParamPort { return p.ParamPort("indexno") }
-func (p *BwaAlign) InReads1() *FilePort      { return p.In("reads_1") }
-func (p *BwaAlign) InReads2() *FilePort      { return p.In("reads_2") }
-func (p *BwaAlign) OutBam() *FilePort        { return p.Out("bam") }
+func (p *BwaAlign) ParamIndexNo() *ParamInPort { return p.ParamInPort("indexno") }
+func (p *BwaAlign) InReads1() *InPort          { return p.In("reads_1") }
+func (p *BwaAlign) InReads2() *InPort          { return p.In("reads_2") }
+func (p *BwaAlign) OutBam() *OutPort           { return p.Out("bam") }
 
 // ----------------------------------------------------------------------------
 // Samtools Merge
 // ----------------------------------------------------------------------------
 
 type SamtoolsMerge struct {
-	*SciProcess
+	*Process
 }
 
 func NewSamtoolsMerge(wf *Workflow, procName string, sampleType string, tmpDir string) *SamtoolsMerge {
@@ -183,15 +176,15 @@ func NewSamtoolsMerge(wf *Workflow, procName string, sampleType string, tmpDir s
 	return &SamtoolsMerge{innerProc}
 }
 
-func (p *SamtoolsMerge) InBams() *FilePort       { return p.In("bams") }
-func (p *SamtoolsMerge) OutMergedBam() *FilePort { return p.Out("mergedbam") }
+func (p *SamtoolsMerge) InBams() *InPort        { return p.In("bams") }
+func (p *SamtoolsMerge) OutMergedBam() *OutPort { return p.Out("mergedbam") }
 
 // ----------------------------------------------------------------------------
 // GATK Mark Duplicates
 // ----------------------------------------------------------------------------
 
 type GATKMarkDuplicates struct {
-	*SciProcess
+	*Process
 }
 
 func NewGATKMarkDuplicates(wf *Workflow, procName string, sampleType string, sampleIndex string, appsdir string, tmpDir string) *GATKMarkDuplicates {
@@ -209,15 +202,15 @@ func NewGATKMarkDuplicates(wf *Workflow, procName string, sampleType string, sam
 	return &GATKMarkDuplicates{innerProc}
 }
 
-func (p *GATKMarkDuplicates) InBam() *FilePort  { return p.In("bam") }
-func (p *GATKMarkDuplicates) OutBam() *FilePort { return p.Out("bam") }
+func (p *GATKMarkDuplicates) InBam() *InPort   { return p.In("bam") }
+func (p *GATKMarkDuplicates) OutBam() *OutPort { return p.Out("bam") }
 
 // ----------------------------------------------------------------------------
 // GATK Realign Create Targets
 // ----------------------------------------------------------------------------
 
 type GATKRealignCreateTargets struct {
-	*SciProcess
+	*Process
 }
 
 func NewGATKRealignCreateTargets(wf *Workflow, procName string, appsdir string, tmpDir string) *GATKRealignCreateTargets {
@@ -236,16 +229,16 @@ func NewGATKRealignCreateTargets(wf *Workflow, procName string, appsdir string, 
 	return &GATKRealignCreateTargets{innerProc}
 }
 
-func (p *GATKRealignCreateTargets) InBamNormal() *FilePort  { return p.In("bamnormal") }
-func (p *GATKRealignCreateTargets) InBamTumor() *FilePort   { return p.In("bamtumor") }
-func (p *GATKRealignCreateTargets) OutIntervals() *FilePort { return p.Out("intervals") }
+func (p *GATKRealignCreateTargets) InBamNormal() *InPort   { return p.In("bamnormal") }
+func (p *GATKRealignCreateTargets) InBamTumor() *InPort    { return p.In("bamtumor") }
+func (p *GATKRealignCreateTargets) OutIntervals() *OutPort { return p.Out("intervals") }
 
 // ----------------------------------------------------------------------------
 // GATK Realign Indels
 // ----------------------------------------------------------------------------
 
 type GATKRealignIndels struct {
-	*SciProcess
+	*Process
 }
 
 func NewGATKRealignIndels(wf *Workflow, procName string, appsdir string, refDir string, tmpDir string) *GATKRealignIndels {
@@ -264,14 +257,14 @@ func NewGATKRealignIndels(wf *Workflow, procName string, appsdir string, refDir 
 			realt={o:realbamtumor};
 			mv $realn.bai ${realn%.bam.tmp}.bai;
 			mv $realt.bai ${realt%.bam.tmp}.bai;`)
-	innerProc.SetPathCustom("realbamnormal", func(t *SciTask) string {
-		path := t.InTargets["bamnormal"].GetPath()
+	innerProc.SetPathCustom("realbamnormal", func(t *Task) string {
+		path := t.InPath("bamnormal")
 		path = strings.Replace(path, ".bam", ".real.bam", -1)
 		path = strings.Replace(path, tmpDir+"/", "", -1)
 		return path
 	})
-	innerProc.SetPathCustom("realbamtumor", func(t *SciTask) string {
-		path := t.InTargets["bamtumor"].GetPath()
+	innerProc.SetPathCustom("realbamtumor", func(t *Task) string {
+		path := t.InPath("bamtumor")
 		path = strings.Replace(path, ".bam", ".real.bam", -1)
 		path = strings.Replace(path, tmpDir+"/", "", -1)
 		return path
@@ -279,18 +272,18 @@ func NewGATKRealignIndels(wf *Workflow, procName string, appsdir string, refDir 
 	return &GATKRealignIndels{innerProc}
 }
 
-func (p *GATKRealignIndels) InBamNormal() *FilePort      { return p.In("bamnormal") }
-func (p *GATKRealignIndels) InBamTumor() *FilePort       { return p.In("bamtumor") }
-func (p *GATKRealignIndels) InIntervals() *FilePort      { return p.In("intervals") }
-func (p *GATKRealignIndels) OutRealBamNormal() *FilePort { return p.In("realbamnormal") }
-func (p *GATKRealignIndels) OutRealBamTumor() *FilePort  { return p.In("realbamtumor") }
+func (p *GATKRealignIndels) InBamNormal() *InPort       { return p.In("bamnormal") }
+func (p *GATKRealignIndels) InBamTumor() *InPort        { return p.In("bamtumor") }
+func (p *GATKRealignIndels) InIntervals() *InPort       { return p.In("intervals") }
+func (p *GATKRealignIndels) OutRealBamNormal() *OutPort { return p.Out("realbamnormal") }
+func (p *GATKRealignIndels) OutRealBamTumor() *OutPort  { return p.Out("realbamtumor") }
 
 // ----------------------------------------------------------------------------
 // GATK Recalibrate
 // ----------------------------------------------------------------------------
 
 type GATKRecalibrate struct {
-	*SciProcess
+	*Process
 }
 
 func NewGATKRecalibrate(wf *Workflow, procName string, sampleType string, appsDir string, refDir string, tmpDir string) *GATKRecalibrate {
@@ -310,15 +303,15 @@ func NewGATKRecalibrate(wf *Workflow, procName string, sampleType string, appsDi
 	return &GATKRecalibrate{innerProc}
 }
 
-func (p *GATKRecalibrate) InRealBam() *FilePort     { return p.In("realbam") }
-func (p *GATKRecalibrate) OutRecalTable() *FilePort { return p.Out("recaltable") }
+func (p *GATKRecalibrate) InRealBam() *InPort      { return p.In("realbam") }
+func (p *GATKRecalibrate) OutRecalTable() *OutPort { return p.Out("recaltable") }
 
 // ----------------------------------------------------------------------------
 // GATK Realign Indels
 // ----------------------------------------------------------------------------
 
 type GATKPrintReads struct {
-	*SciProcess
+	*Process
 }
 
 func NewGATKPrintReads(wf *Workflow, procName string, sampleType string, appsdir string, refDir string) *GATKPrintReads {
@@ -337,5 +330,5 @@ func NewGATKPrintReads(wf *Workflow, procName string, sampleType string, appsdir
 	return &GATKPrintReads{innerProc}
 }
 
-func (p *GATKPrintReads) InRealBam() *FilePort   { return p.In("realbam") }
-func (p *GATKPrintReads) OutRecalBam() *FilePort { return p.Out("recalbam") }
+func (p *GATKPrintReads) InRealBam() *InPort    { return p.In("realbam") }
+func (p *GATKPrintReads) OutRecalBam() *OutPort { return p.Out("recalbam") }
