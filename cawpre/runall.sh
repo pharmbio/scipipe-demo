2692d0ac4544675db9015ee65f3314b2c009db6e
Samuel Lampa <samuel.lampa@gmail.com>
2018-05-25 19:56:56 +0200

    Add module load statements

--- cawpre/runall.sh
+++ cawpre/runall.sh
@@ -3,6 +3,9 @@
 #SBATCH -p node
 #SBATCH -t 1-00:00:00
 #SBATCH -J cawpre_scipipe_test
+module load bioinfo-tools
+module load samtools/1.8
+module load bwa/0.7.17
 binname=cawpre_$(date +%Y%m%d_%H%M%S)
 go build -o $binname
 ./$binname -maxtasks 11 -procs print_reads_.*
